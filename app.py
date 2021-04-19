909c1bb037746e2ca500dda6fd92a22f5ceab965
mauri1996 <mauri1996@hotmail.es>
2021-04-18 20:08:01 -0500

    add not user Mercado libre

--- app.py
+++ app.py
@@ -15,34 +15,8 @@ app= Flask(__name__)
 CORS(app)
 @app.route('/search', methods = ['GET'])
 def MercadoLibre():
-    URL = request.args.get('url')
-    # fase 1 - Datos generales del articulo
-    page = requests.get(URL)
-    soup = BeautifulSoup(page.content, 'html.parser')
-
-    NombreArticulo = soup.find_all("h1", {"class": "ui-pdp-title"})[0].contents[0]
-    NombreArticulo= NombreArticulo.replace("/"," ")
-    precio = soup.find_all("span", {"class": "price-tag-fraction"})[0].contents[0]        
-    Image_articulo = soup.find_all("figure", {"class": "ui-pdp-gallery__figure"})[0]
-    Image_articulo= Image_articulo.find_all("img")[0]['data-zoom']
     
-    ## si encuentra o no un modal en la 2 version de mercado libre
-    ## Datos del vendedor
-    try:
-        # fase 2
-        url = soup.find_all("a", {"class": "ui-pdp-media__action ui-box-component__action"})[0]['href']
-    
-    except:
-        url = soup.find_all("a", {"class": "ui-pdp-action-modal__link"})[-1]['href']       
-        # fase 2
-        page_user_middleware = requests.get(url)
-        soup_user_middleware = BeautifulSoup(page_user_middleware.content, 'html.parser')
-        
-        data = soup_user_middleware.find_all("p", {"class": "feedback-profile-link"})[0]
-        url = data.find_all("a")[0]
-        url = url['href']
-
-    finally:
+    def fase2(url):
         # fase 2.1
         page_user = requests.get(url)
         soup_user = BeautifulSoup(page_user.content, 'html.parser')
@@ -61,43 +35,82 @@ def MercadoLibre():
         time = soup_user.find_all("p", {"class": "data-level__description"})[-1]
         time = time.find_all("span")[0].contents[-1].split(' ')[1]
         if(time == 'años'):
-            time = 'anios'      
+            time = 'anios'
 
-    # fase 3 - Comparativa con los demas articulos parecidos
-    new_url = 'https://listado.mercadolibre.com.ec/'+NombreArticulo
-    page_other_article = requests.get(new_url)
-    soup_other_article = BeautifulSoup(page_other_article.content, 'html.parser')
-    card = soup_other_article.find_all("img", {"class": "ui-search-result-image__element"})
-    datos = []
-    sume = 0
-    maxi = 0
-    mini = 9999999
+        return [time,calification_points,recomendado,ventas_completadas,años_vendiendo,name_vendedor]
+           
+    def buscarInfo(NombreArticulo):
+        # fase 3 - Comparativa con los demas articulos parecidos
+        new_url = 'https://listado.mercadolibre.com.ec/'+NombreArticulo
+        page_other_article = requests.get(new_url)
+        soup_other_article = BeautifulSoup(page_other_article.content, 'html.parser')
+        card = soup_other_article.find_all("img", {"class": "ui-search-result-image__element"})
+        datos = []
+        sume = 0
+        maxi = 0
+        mini = 9999999
 
-    for item in card:
-        # obtener precios
-        price = soup_other_article.find_all("a", {"title": item['alt']})
-        #print(price[1].find_all("span", {"class": "price-tag-fraction"}))
-        try:
-            num = float(price[1].find_all("span", {"class": "price-tag-fraction"})[0].contents[0])        
-        except:
-            search = soup_other_article.find_all("div", {"class": "andes-card andes-card--flat andes-card--default ui-search-result ui-search-result--core andes-card--padding-default"})
-            for c in search:
-                if (item['alt'] == c.contents[0].contents[0]['title']):
-                    num = float(c.contents[1].find_all("span", {"class": "price-tag-fraction"})[0].contents[0])
-        finally:
-            sume = sume + num
-            if(num<mini):
-                mini=num
-            if (num>maxi):
-                maxi=num
-            ## generacion de json
-            datos.append({'Nombre': item['alt'],  'Url': price[0]['href'], 'Image': item['data-src'], 'Precio': num})            
-    average= sume/len(card)
-    average = round(average,2) 
+        for item in card:
+            # obtener precios
+            price = soup_other_article.find_all("a", {"title": item['alt']})
+            #print(price[1].find_all("span", {"class": "price-tag-fraction"}))
+            try:
+                num = float(price[1].find_all("span", {"class": "price-tag-fraction"})[0].contents[0])        
+            except:
+                search = soup_other_article.find_all("div", {"class": "andes-card andes-card--flat andes-card--default ui-search-result ui-search-result--core andes-card--padding-default"})
+                for c in search:
+                    if (item['alt'] == c.contents[0].contents[0]['title']):
+                        num = float(c.contents[1].find_all("span", {"class": "price-tag-fraction"})[0].contents[0])
+            finally:
+                sume = sume + num
+                if(num<mini):
+                    mini=num
+                if (num>maxi):
+                    maxi=num
+                ## generacion de json
+                datos.append({'Nombre': item['alt'],  'Url': price[0]['href'], 'Image': item['data-src'], 'Precio': num})            
+        average= sume/len(card)
+        average = round(average,2) 
+        return [average,mini,maxi,datos]
 
-    dicJson = {"Nombre" : NombreArticulo,"Image":Image_articulo,"Vendedor":name_vendedor,"Precio":precio ,"Puntos":calification_points, "Recomendado": recomendado, "Ventas":ventas_completadas,"Time":años_vendiendo,"typeTime":time,"Promedio":average, "Maximo":maxi,"Minimo": mini,'otrosDatos': datos}
+    URL = request.args.get('url')
+    # fase 1 - Datos generales del articulo
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    NombreArticulo = soup.find_all("h1", {"class": "ui-pdp-title"})[0].contents[0]
+    NombreArticulo= NombreArticulo.replace("/"," ")
+    precio = soup.find_all("span", {"class": "price-tag-fraction"})[0].contents[0]        
+    Image_articulo = soup.find_all("figure", {"class": "ui-pdp-gallery__figure"})[0]
+    Image_articulo= Image_articulo.find_all("img")[0]['data-zoom']
     
-    return json.dumps(dicJson)
+    ## si encuentra o no un modal en la 2 version de mercado libre
+    ## Datos del vendedor
+    try:
+        # fase 2
+        url = soup.find_all("a", {"class": "ui-pdp-media__action ui-box-component__action"})[0]['href']
+        fas2 = fase2(url)
+        fas3 = buscarInfo(NombreArticulo)
+
+    except:        
+        url = soup.find_all("a", {"class": "ui-pdp-action-modal__link"})[-1]['href']
+        # fase 2
+        page_user_middleware = requests.get(url)
+        soup_user_middleware = BeautifulSoup(page_user_middleware.content, 'html.parser')
+        try:
+            data = soup_user_middleware.find_all("p", {"class": "feedback-profile-link"})[0]
+            url = data.find_all("a")[0]
+            url = url['href']
+            fas2 = fase2(url)
+            fas3 = buscarInfo(NombreArticulo)
+
+        except:
+            fas2 = [0,0,0,0,0,""]
+            fas3 = buscarInfo(NombreArticulo)
+
+    finally:        
+        dicJson = {"Nombre" : NombreArticulo,"Image":Image_articulo,"Vendedor":fas2[5],"Precio":precio ,"Puntos":fas2[1], "Recomendado": fas2[2], "Ventas":fas2[3],"Time":fas2[4],"typeTime":fas2[0],"Promedio":fas3[0], "Maximo":fas3[2],"Minimo": fas3[1],'otrosDatos': fas3[3]}
+        return json.dumps(dicJson)
 
 ## Busqueda en Ebay
 @app.route('/searchEbay', methods = ['GET'])
@@ -140,8 +153,7 @@ def Ebay():
         average= sume/len(datos)
         average = round(average,2) 
 
-        dicJson = dicJson = {"Promedio":average, "Maximo":maxi,"Minimo": mini, "otrosDatos":datos}
-        
+        dicJson = dicJson = {"Promedio":average, "Maximo":maxi,"Minimo": mini, "otrosDatos":datos}        
         return json.dumps(dicJson),200
     except:
         dicJson = {}
